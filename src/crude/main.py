e2c293ed76fe21ff79eb19512c5a008d7ede4bf5
Charlie <charlieavery123@hotmail.co.uk>
2024-08-19 19:28:14 +0100

    folder structure

--- src/crude/main.py
+++ src/crude/main.py
@@ -2,125 +2,7 @@ import os
 from dotenv import load_dotenv
 import dlt
 
-from rest_api import (
-    rest_api_source,
-)
-
-
-load_dotenv(override=True)
-
-
-eia_source = rest_api_source(
-    {
-        "client": {
-            "base_url": "https://api.eia.gov/v2/",
-            "headers": {},
-        },
-        "resource_defaults": {
-            "endpoint": {
-                "params": {
-                    "api_key": os.getenv("API_KEY"),
-                    "offset": "0",
-                    # "length": "10000",
-                    "frequency": "monthly",
-                    "start": "2000-01",
-                    # "sort": [
-                    #     {
-                    #         "column": "period",
-                    #         "direction": "desc",
-                    #     }
-                    # ],
-                },
-            },
-        },
-        "resources": [
-            {
-                "name": "imports",
-                "endpoint": {
-                    "path": "crude-oil-imports/data",
-                    "params": {},
-                },
-            },
-            {
-                "name": "prices",
-                "endpoint": {
-                    "path": "petroleum/pri/spt/data/",
-                    "params": {
-                        "facets[product][]": "EPCWTI",  # only WTI prices
-                    },
-                },
-            },
-            {
-                "name": "production",
-                "endpoint": {
-                    "path": "petroleum/crd/crpdn/data",
-                    "params": {},
-                },
-            },
-            {
-                "name": "net_imports",
-                "endpoint": {
-                    "path": "petroleum/move/neti/data",
-                    "params": {
-                        "facets[product][]": "EPC0",  # only crude oil
-                    },
-                },
-            },
-            {
-                "name": "stocks",
-                "endpoint": {
-                    "path": "petroleum/stoc/wstk/data",
-                    "params": {
-                        "frequency": "weekly",
-                        "facets[product][]": "EPC0",  # only crude oil
-                        "facets[series][]": "WCRSTUS1",  # entire US
-                    },
-                },
-            },
-            {
-                "name": "refinery_stocks",
-                "endpoint": {
-                    "path": "petroleum/stoc/ref/data",
-                    "params": {
-                        "facets[product][]": "EPC0",  # only crude oil
-                    },
-                },
-            },
-            {
-                "name": "refinery_net_input",
-                "endpoint": {
-                    "path": "petroleum/pnp/wiup/data",
-                    "params": {
-                        "frequency": "weekly",
-                        "facets[product][]": "EPC0",  # only crude oil
-                    },
-                },
-            },
-            {
-                "name": "supply_estimates",
-                "endpoint": {
-                    "path": "petroleum/sum/sndw/data",
-                    "params": {
-                        "frequency": "weekly",
-                        "facets[product][]": "EPC0",  # only crude oil
-                        "facets[process][]": "FPF",  # field production
-                    },
-                },
-            },
-            {
-                "name": "international_production",
-                "endpoint": {
-                    "path": "international/data",
-                    "params": {
-                        "facets[productId][]": "57",  # only crude oil
-                        "facets[activityId][1]": "1",  # production
-                        "facets[activityId][2]": "2",  # or consumption
-                    },
-                },
-            },
-        ],
-    }
-)
+from .sources import eia
 
 
 def main():
@@ -131,7 +13,7 @@ def main():
         progress="log",
     )
 
-    load_info = pipeline.run(eia_source)
+    load_info = pipeline.run(eia.source)
     print(load_info)
 
 
